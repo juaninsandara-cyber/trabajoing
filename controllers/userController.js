c597aa68df8cc2ab686216af1b3956014f5b079e
juaninsandara-cyber <juaninsandara@unicomfacauca.edu.co>
2025-10-04 16:10:53 -0500

    v6

--- controllers/userController.js
+++ controllers/userController.js
@@ -4,39 +4,52 @@ exports.login = async (req, res) => {
   const { username, password } = req.body;
 
   try {
-    const user = await User.findOne({ where: { username, password } });
+    // Buscar usuario solo por  su nombre 
+    const user = await User.findOne({ where: { username } });
 
-    if (user) {
-      return res.status(200).json({ token: 'TODO CONECTADO' });// todo esta bien 
+    // no existe el usuario
+    if (!user) {
+      return res.status(401).json({ message: 'Usuario no registrado' });
     }
 
-    return res.status(401).json({ message: 'usuario No Registrado' });
+    // Si esixte el usuario pero la contraseña no es correcta
+    if (user.password !== password) {
+      return res.status(401).json({ message: 'Contraseña incorrecta' });
+    }
+
+    // Si todo esta bien 
+    return res.status(200).json({
+      message: 'Usuario registrado correctamente',
+      token: 'TODO CONECTADO '
+    });
+
   } catch (error) {
+    console.error(' Error en login:', error);
     return res.status(500).json({ message: 'Error del servidor', error });
   }
 };
+
+
 exports.register = async (req, res) => {
   try {
     const { username, password } = req.body;
 
-    // Validar si ya existe un usuario con ese username
+    // si ya existe un usuario con ese nombre 
     const existingUser = await User.findOne({ where: { username } });
     if (existingUser) {
       return res.status(400).json({ message: 'El usuario ya existe' });
     }
 
-    // Crear nuevo usuario
-    const newUser = await User.create({
-      username,
-      password
-    });
+    // Crear un nuevo usuario
+    const newUser = await User.create({ username, password });
 
-    res.status(201).json({
+    return res.status(201).json({
       message: 'Usuario registrado con éxito',
       user: newUser
     });
+
   } catch (error) {
-    console.error(error);
-    res.status(500).json({ message: 'Error en el servidor', error });
+    console.error('❌ Error en registro:', error);
+    return res.status(500).json({ message: 'Error del servidor', error });
   }
 };
