d9a0103c86f0ec5e739cd29ac0c494f9a32446c9
[Felipe_bolaños] <[duvanbolanos@unicomfacauca.edu.co]>
2025-11-10 20:04:37 -0500

    Subiendo proyecto a mi rama FELIPE

--- models/user.js
+++ models/user.js
@@ -1,18 +1,25 @@
-const { DataTypes } = require('sequelize');
-const sequelize = require('../config/database');
+'use strict';
 
-const User = sequelize.define('User', {
-  username: {
-    type: DataTypes.STRING,
-    allowNull: false,
-    unique: true
-  },
-  password: {
-    type: DataTypes.STRING,
-    allowNull: false
-  }
-}, {
-  timestamps: true // createdAt y updatedAt
-});
+module.exports = (sequelize, DataTypes) => {
+  const User = sequelize.define('User', {
+    username: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false
+    }
+  }, {
+    tableName: 'users',
+    timestamps: true
+  });
 
-module.exports = User;
+  User.associate = function(models) {
+    // Aquí van relaciones si las necesitas
+    // por ejemplo: User.hasMany(models.Payment)
+  };
+
+  return User;
+};
