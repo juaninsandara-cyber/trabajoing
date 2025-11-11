0001bb465e0d8743b283f003422fb991543a24d8
juaninsandara-cyber <juaninsandara@unicomfacauca.edu.co>
2025-11-10 22:40:58 -0500

    ultimas

--- config/database.js
+++ config/database.js
@@ -1,8 +1,17 @@
+require('dotenv').config();
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize('parqueadero_db', 'root', 'root', {
-  host: '127.0.0.1',
-  dialect: 'mysql'
-});
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASS,
+  {
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    dialect: 'mysql',
+    logging: false, // quita logs de SQL en consola
+  }
+);
 
 module.exports = sequelize;
+
